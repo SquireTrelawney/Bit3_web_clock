62ac043e9959f9723a1ea802e70f10578ac4903d
devSquire <devssquire@gmail.com>
2021-10-31 14:58:15 +0300

    added clock hands

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ circle.arc(300, 300, 290, 0, 2 * Math.PI);
 
 function drawClock(){
 
+    context.clearRect(0, 0, 300, 300)
+
     context.strokeStyle = "orange";
     context.lineWidth = 15;
     context.fill(circle);
@@ -29,13 +31,67 @@ function drawClock(){
         line.lineTo(qX, qY);
         if(d % 5 == 0){
             context.lineWidth = 15;
+            context.font = "30px serif";
+            context.fillStyle = "orange";
+
+            if (d == 15){
+                context.fillText(12, qX * 0.9 + 20, qY * 0.9 + 40);
+            }else{
+                context.fillText((12 - (d / 5) + 3) % 12 , qX * 0.9 + 20, qY * 0.9 + 40);
+            }
+
+            context.fillStyle = "black";
         }else{
             context.lineWidth = 3;
         }
         context.stroke(line);
     }
+    var date = new Date();
+    var seconds = date.getSeconds();
+    var minutes = date.getMinutes();
+    var hours = date.getHours();
+
+  
+
+   
+
+    var secAngle = ((hours % 12 / 12) * (2 * Math.PI));
+    context.strokeStyle = "orange";
+    context.lineWidth = 15;
+    var sPX = Math.sin(secAngle) * R * 0.55 + R + 10;
+    var sPY = -Math.cos(secAngle) * R * 0.55 + R + 10;
+    var sQX = R + 10;
+    var sQY = R + 10;
+    var sLine = new Path2D();
+    sLine.moveTo(sQX, sQY);
+    sLine.lineTo(sPX, sPY);
+    context.stroke(sLine);
+
+    var secAngle = ((minutes / 60) * (2 * Math.PI));
+    context.strokeStyle = "orange";
+    context.lineWidth = 10;
+    var sPX = Math.sin(secAngle) * R * 0.75 + R + 10;
+    var sPY = -Math.cos(secAngle) * R * 0.75 + R + 10;
+    var sQX = R + 10;
+    var sQY = R + 10;
+    var sLine = new Path2D();
+    sLine.moveTo(sQX, sQY);
+    sLine.lineTo(sPX, sPY);
+    context.stroke(sLine);
+
+    var secAngle = ((seconds / 60) * (2 * Math.PI));
+    context.strokeStyle = "red";
+    context.lineWidth = 5;
+    var sPX = Math.sin(secAngle) * R * 0.95 + R + 10;
+    var sPY = -Math.cos(secAngle) * R * 0.95 + R + 10;
+    var sQX = R + 10;
+    var sQY = R + 10;
+    var sLine = new Path2D();
+    sLine.moveTo(sQX, sQY);
+    sLine.lineTo(sPX, sPY);
+    context.stroke(sLine);
 
 
-    
+    setTimeout(drawClock, 1000)
 }
 drawClock();
